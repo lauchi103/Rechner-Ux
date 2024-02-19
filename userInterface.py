245d6f511f38e70aeb24f55d90fd8ae543da9ccf
lauchi103 <louisdaslauch1@gmail.com>
2024-02-19 22:20:41 +0100

    Load Images

--- userInterface.py
+++ userInterface.py
@@ -1,3 +1,19 @@
 import tkinter as tk
+import os
 import customtkinter as ctk
+from PIL import Image
 
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Matrix-Rechner")
+        self.geometry("720x480")
+
+        #set grid layout 4x6
+        self.grid_rowconfigure(3,weight=1)
+        self.grid_columnconfigure(5,weight=1)
+
+        #load images
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+        self.cube_image = ctk.CTkImage
