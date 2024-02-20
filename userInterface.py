7c4126986c97203c27b62ca2f92c1d5005c4b5cc
lauchi103 <louisdaslauch1@gmail.com>
2024-02-20 18:44:41 +0100

    Matrix input

--- userInterface.py
+++ userInterface.py
@@ -22,7 +22,7 @@ class App(ctk.CTk):
 
         #sidebar frame
         self.navigation_frame = ctk.CTkFrame(master=self,corner_radius=0)
-        self.navigation_frame.grid(row=0,column=0,columnspan=4,sticky="nsew")
+        self.navigation_frame.grid(row=0,column=0,sticky="nsew")
         self.navigation_frame.rowconfigure(4,weight=1)
         self.navigation_frame_width = self.navigation_frame.cget("width")
         
@@ -32,20 +32,19 @@ class App(ctk.CTk):
         self.matrix_multipl_button = ctk.CTkButton(master=self.navigation_frame,corner_radius=0,height=40,border_spacing=10,
                                            fg_color="transparent",hover_color=("gray70","gray30"),text="Matrix",
                                            text_color=("gray30","gray90"),image=self.image_cube,anchor="w",command=self.matrix_button_event)
-        self.matrix_multipl_button.grid(rows=1,columns=0,sticky="ew")
+        self.matrix_multipl_button.grid(row=1,column=0,sticky="ew")
         self.matrix_multipl_button.configure(width=self.navigation_frame_width)
 
         self.systemth_button = ctk.CTkButton(master=self.navigation_frame,corner_radius=0,height=40,border_spacing=10,
                                            fg_color="transparent",hover_color=("gray70","gray30"),text="Systemth.",
                                            text_color=("gray30","gray90"),image=self.image_systemth,anchor="w",command=self.systemth_button_event)
-        self.systemth_button.grid(rows=2,columns=0,sticky="ew")
+        self.systemth_button.grid(row=2,column=0,sticky="ew")
         self.systemth_button.configure(width=self.navigation_frame_width)
 
         #Matrix Frame
         self.matrix_multipl_frame = Matrix_multipl_Frame(self)
-        #frame2 in sidebar
-        self.frame2_sidebar = ctk.CTkFrame(master=self.navigation_frame,corner_radius=0,fg_color="green")
-        self.frame2_sidebar.grid(row=3,column=0,sticky="nsew")
+        self.matrix_multipl_frame.grid(row=0,column=1,sticky="nsew")
+
 
         self.select_frame_by_name("matrix")#bearbeiten
 
@@ -65,6 +64,7 @@ class Matrix_multipl_Frame(ctk.CTkFrame):
 
         self.master = master
         self._corner_radius = 0
+        self.configure( fg_color = "transparent" )
 
         self.row1=""
         self.column1=""
@@ -74,11 +74,12 @@ class Matrix_multipl_Frame(ctk.CTkFrame):
         self.matrix1_columns = ctk.CTkEntry(master=self,textvariable=self.column1,height=30,placeholder_text="Columns M1")
         self.matrix2_rows = ctk.CTkEntry(master=self,textvariable=self.row2,height=30,placeholder_text="Rows M2")
         self.matrix2_columns = ctk.CTkEntry(master=self,textvariable=self.column2,height=30,placeholder_text="Columns M2")
-        self.matrix1_rows.grid(rows=0,columns=0,sticky= "nsew")
-        self.matrix1_columns.grid(rows=0,columns=1,sticky="nsew")
-        self.matrix2_rows.grid(rows=1,columns=0,sticky="nsew")
-        self.matrix2_columns.grid(rows=1,columns=1,sticky="nsew")
-
+        self.rowconfigure(1,weight=1)
+        self.columnconfigure(1,weight=1)
+        self.matrix1_rows.grid(row=0,column=0 , padx = 20 , pady = 20, sticky = "wn")
+        self.matrix1_columns.grid(row=0,column=1 , padx = 20 , pady = 20, sticky = "wn")
+        self.matrix2_rows.grid(row=1,column=0 , padx = 20 , pady = 20, sticky = "wn")
+        self.matrix2_columns.grid(row=1,column=1 , padx = 20 , pady = 20, sticky = "wn")
 
 
     def create_matrix_input(self,rows_input, columns_input, frame):
